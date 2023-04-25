59e8eb456ef0a87f899064933fdea51b4514a5b4
amolhase <amolhase110@gmail.com>
2023-04-25 23:22:22 +0530

    Encapsulation Assignment

---
+++ src/com/assignment/encapsulation/Entry.java
@@ -0,0 +1,12 @@
+package com.assignment.encapsulation;
+
+public class Entry {
+
+	public static void main(String[] args) {
+		// DD-MM-YYY
+
+		Date date = new Date(78, 98, 2090);
+		System.out.println(date);
+	}
+	
+}
